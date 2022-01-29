f5fa93c62e61d807c5c4e9bfaddd2baaf3d152fc
AmitGitO <amitoren85@gmail.com>
2022-01-29 19:49:55 +0200

    Amit-29.01.22 TMPro, Coins, Score UI
    added TextMeshPro
    Coins and coins script
    Score script and UI

---
+++ PIG_Final_Project_V01/Assets/Scripts/Collectable.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//great name!
+public class Collectable : MonoBehaviour
+{
+    //Coin Value
+    public int value = 5;
+    //Coin rotation
+    public Vector2 rotation;
+    //Rotation speed
+    public float speed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Coin rotation speed
+        transform.Rotate(rotation * speed * Time.deltaTime);
+    }
+
+}
---
+++ PIG_Final_Project_V01/Assets/Scripts/PlayerScore.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+
+public class PlayerScore : MonoBehaviour
+{
+    public int score = 0;
+    public TextMeshProUGUI _score;
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = 0;
+        _score.text = "Score: " + score;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Collectable collectable = other.GetComponent<Collectable>();
+        if (collectable)
+        {
+            score += collectable.value;
+            _score.text = "Score: " + score;
+
+            Debug.Log("Score Is " + score);
+            other.gameObject.SetActive(false);
+        }
+    }
+}

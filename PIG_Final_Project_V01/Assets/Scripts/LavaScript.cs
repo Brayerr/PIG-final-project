e4049cdceb74c671b5f185e51d903613414b3a6e
Brayerr <brayer_alon@walla.com>
2022-02-11 19:19:26 +0200

    11.2
    lava script added with dmg to player.

---
+++ PIG_Final_Project_V01/Assets/Scripts/LavaScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LavaScript : MonoBehaviour
+{
+    //player refference
+    Player player;
+    //initializing lava damage
+    public int damage = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //player script reffrence
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //when colliding with lava
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        //player takes damage from lava
+        player.TakeDamage(damage);
+    }
+}

2865da0ec82082003388e3447c85b9100e76f437
TalTsadikov <93190133+TalTsadikov@users.noreply.github.com>
2022-02-14 14:47:24 +0200

    Main Menu
    Created a main menu 14.2

---
+++ PIG_Final_Project_V01/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    //Method to load first level when play button is pressed
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    //Method to close the game in case the quit button was pressed
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}

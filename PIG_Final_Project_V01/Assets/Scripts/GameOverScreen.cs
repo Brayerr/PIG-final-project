6f8d0003b7838585d037f40c5421aa22d40626a7
TalTsadikov <93190133+TalTsadikov@users.noreply.github.com>
2022-02-16 20:38:00 +0200

    Game over Added
    Game over Added 16.2

---
+++ PIG_Final_Project_V01/Assets/Scripts/GameOverScreen.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverScreen : MonoBehaviour
+{
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void ReturnToMainMenu()
+    {        
+        SceneManager.LoadScene(0);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}

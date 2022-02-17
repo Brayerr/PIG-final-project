a7143963fd73606b7bb999cde84905a940ac7013
TalTsadikov <93190133+TalTsadikov@users.noreply.github.com>
2022-02-17 13:42:54 +0200

    Life UI and system added
    Life UI and system added 17.2

---
+++ PIG_Final_Project_V01/Assets/Scripts/LivesController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LivesController : MonoBehaviour
+{
+    [SerializeField] private Player player;
+    [SerializeField] private Image totalLivesBar;
+    [SerializeField] private Image currentLivesBar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        totalLivesBar.fillAmount = player.currentLives / 10;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentLivesBar.fillAmount = player.currentLives / 10;
+    }
+}
--- PIG_Final_Project_V01/Assets/Scripts/Player.cs
+++ PIG_Final_Project_V01/Assets/Scripts/Player.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
 {
@@ -8,6 +9,10 @@ public class Player : MonoBehaviour
     public float playerMaxHealth = 5;
     //player current health
     public float currentHealth;
+    //player max lives
+    public float playerMaxLives = 3;
+    //player current lives
+    public float currentLives;
     //checks if player is dead
     public bool playerIsDead = false;
     //checks if player can take damage
@@ -22,6 +27,8 @@ public class Player : MonoBehaviour
     {
         //setting player health to the max
         currentHealth = playerMaxHealth;
+        //setting player Lives to the max
+        currentLives = playerMaxLives;
         //setting up checkpoint values as level start values
         checkPoint = new Vector3(-7.04f, -3.27f, transform.position.z);
     }
@@ -36,17 +43,26 @@ public class Player : MonoBehaviour
     //function that checks player health
     public void PlayerDead(float currentHP)
     {
-        //if player health hits 0
-        if (currentHP <= 0)
+        if (currentLives >= 0)
         {
-            //tells bool that player is dead
-            playerIsDead = true;
-            //resets player position to level start position
-            transform.position = checkPoint;                
-            //resets player health to max health
-            currentHealth = playerMaxHealth;
-            //feedback to console
-            Debug.Log("You have died, restarting level.");
+            //if player health hits 0
+            if (currentHP <= 0)
+            {
+                //tells bool that player is dead
+                playerIsDead = true;
+                //player died so removing 1 life
+                currentLives--;
+                //resets player position to level start position
+                transform.position = checkPoint;
+                //resets player health to max health
+                currentHealth = playerMaxHealth;
+                //feedback to console
+                Debug.Log("You have died, restarting level.");
+            }
+        }
+        else
+        {
+            SceneManager.LoadScene(4);
         }
     }
 

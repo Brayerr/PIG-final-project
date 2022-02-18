20845c541925875f27cbebb5c64d0466d0a99ae9
AmitGitO <amitoren85@gmail.com>
2022-02-18 20:34:49 +0200

    Moving Platfrom and attach script

---
+++ PIG_Final_Project_V01/Assets/Scripts/MovePlatforms.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlatforms : MonoBehaviour
+{
+    public GameObject[] waypoints;
+    public int current = 0;
+    public float speed;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Vector3.Distance(waypoints[current].transform.position, transform.position) < 0.1)
+        {
+            //if arrived at waypoint go to next on list
+            current++;
+            //if arrived at last waypoint on list go to first
+            if(current >= waypoints.Length)
+            {
+                current = 0;
+            }
+        }
+        // move position of platform towards next waypoint in certain speed
+        transform.position = Vector3.MoveTowards(transform.position, waypoints[current].transform.position, Time.deltaTime * speed);
+    }
+}

b9cc4fe2468ac1898b63ac6b95d6eae1579ef2d1
Ellie2405 <mttne2405@gmail.com>
2022-02-19 02:51:37 +0200

    Level1 0.9
    traps almost all done,

---
+++ PIG_Final_Project_V01/Assets/Scripts/Obstacles Scripts/SpikeCannon.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpikeCannon : MonoBehaviour
+{
+    public GameObject projectile;
+    public float speed;
+    public float firingDelay;
+    private bool readyToFire = true;
+    Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (readyToFire)
+        {
+
+            GameObject go = Instantiate(projectile, transform.position,projectile.transform.rotation);
+            rb = go.GetComponent<Rigidbody2D>();
+            rb.velocity = Vector2.right * speed;
+            StartCoroutine(Cooldown());
+            readyToFire = false;
+        }
+    }
+
+    IEnumerator Cooldown()
+    {
+        yield return new WaitForSeconds(firingDelay);
+        readyToFire = true;
+    }
+
+}

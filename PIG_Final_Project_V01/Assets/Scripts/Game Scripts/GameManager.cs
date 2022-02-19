4af5d0ff7c264ffab37232a94d3fe3d857783cc5
Brayerr <brayer_alon@walla.com>
2022-02-19 23:10:58 +0200

    game manage for all + pause menue, level select screen

--- PIG_Final_Project_V01/Assets/Scripts/Game Scripts/GameManager.cs
+++ PIG_Final_Project_V01/Assets/Scripts/Game Scripts/GameManager.cs
@@ -6,18 +6,24 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    public PauseMenu pm;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Escape))
+        if (Input.GetKeyDown(KeyCode.Escape))
         {
-            
+            pm.Pause();
+        }
+
+        if (Input.GetKeyDown(KeyCode.F1))
+        { 
+            pm.Resume();
         }
     }
 }
--- PIG_Final_Project_V01/Assets/Scripts/Game Scripts/PauseMenu.cs
+++ PIG_Final_Project_V01/Assets/Scripts/Game Scripts/PauseMenu.cs
@@ -1,9 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class PauseMenu : MonoBehaviour
 {
+    public PauseMenu pm;
+
+    public bool pauseMenuOpen = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +20,29 @@ public class PauseMenu : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void Pause()
+    {
+        this.gameObject.SetActive(true);
+        Time.timeScale = 0f;
+        pauseMenuOpen = true;
+        Debug.Log("pause menu opened");
+
+    }
+
+    public void Resume()
+    {
+        this.gameObject.SetActive(false);
+        Time.timeScale = 1f;
+        pauseMenuOpen = false;
+        Debug.Log("pause menu closed");
+    }
+
+    public void MainMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Main Menu");
+        Debug.Log("back to main menu");
     }
 }

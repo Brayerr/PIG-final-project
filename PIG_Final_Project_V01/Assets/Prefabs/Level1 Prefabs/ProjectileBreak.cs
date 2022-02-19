0507620ca4a05073ecb3bf8338f9cc7eb8445383
Ellie2405 <mttne2405@gmail.com>
2022-02-20 01:49:52 +0200

    2 small scripts edited
    added documentation

--- PIG_Final_Project_V01/Assets/Prefabs/Level1 Prefabs/ProjectileBreak.cs
+++ PIG_Final_Project_V01/Assets/Prefabs/Level1 Prefabs/ProjectileBreak.cs
@@ -5,10 +5,13 @@ using UnityEngine;
 //a simple script to make projectiles break after hitting objects
 public class ProjectileBreak : MonoBehaviour
 {
+    //for hitting player
     private void OnCollisionEnter2D(Collision2D collision)
     {
         Destroy(this.gameObject, 0.2f);
     }
+
+    //for hitting wall or shredder
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("ProjShredder"))
--- PIG_Final_Project_V01/Assets/Scripts/Obstacles Scripts/SwitchDoor.cs
+++ PIG_Final_Project_V01/Assets/Scripts/Obstacles Scripts/SwitchDoor.cs
@@ -2,29 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//script for a button that triggers a "door" barricade
 public class SwitchDoor : MonoBehaviour
 {
-    public GameObject door;
-    public Transform button;
+    public GameObject door;     //track door object
+    public Transform button;    //track button
 
-
-    // Start is called before the first frame update
     void Start()
     {
+        //assign door and button objects
         door = gameObject.transform.GetChild(0).gameObject;
         button = this.gameObject.transform;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Debug.Log("bruv");
+        //remove the barricade object
         door.SetActive(false);
+        //change button shape to look pressed
         button.localScale = (new Vector3(1, 0.1f, 1));
         button.position = new Vector3(button.position.x,button.position.y - 0.1f,button.position.z);
     }
